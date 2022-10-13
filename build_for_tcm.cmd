8ea341a5e80c5724d40621a16f0dd402da795ac6
AlexT2052 <alextong2004@gmail.com>
2022-10-12 23:30:48 -0500

    Looks to be working fully... and cross platform...

--- build_for_tcm.cmd
+++
@@ -1,9 +0,0 @@
-cd ..
-SET var=%cd%
-for %%I in (.) do set ProjDirName=%%~nxI
-cd ..							
-cd gophercan-lib/gophercannon
-python gcan_auto_gen.py networks/go4-22c.yaml
-cd ../..
-cd Gopher_Sense
-python sensor_cannon.py %var%/%ProjDirName%_config.yaml
