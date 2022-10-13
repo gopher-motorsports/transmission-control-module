9b43c8831bc6d49cf16c1e445790bc6819c4418b
AlexT2052 <alextong2004@gmail.com>
2022-10-12 22:01:22 -0500

    Fix - was a bit too confident those comments wouldn't break everything lmao

--- build_for_tcm.cmd
+++ build_for_tcm.cmd
@@ -1,9 +1,9 @@
-cd .. 							&:: Get out of the Debug folder
-SET var=%cd%						&:: Store current path in var
-for %%I in (.) do set ProjDirName=%%~nxI		&:: Get name of current directory
+cd ..
+SET var=%cd%
+for %%I in (.) do set ProjDirName=%%~nxI
 cd ..							
 cd gophercan-lib/gophercannon
-python gcan_auto_gen.py networks/go4-22c.yaml		&:: Run gcan_auto_gen with car configuration argument
+python gcan_auto_gen.py networks/go4-22c.yaml
 cd ../..
 cd Gopher_Sense
-python sensor_cannon.py %var%/%ProjDirName%_config.yaml	&:: Run sensor_cannon with config.yaml from project directory
+python sensor_cannon.py %var%/%ProjDirName%_config.yaml
