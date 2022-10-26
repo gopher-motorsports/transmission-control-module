6fff5067235db8c3f8f64a43c994ee77685b6232
Calvin Molitor <52789184+caljay98@users.noreply.github.com>
2022-10-26 18:47:12 -0500

    lots o work to car_utils

--- Core/Inc/car_utils.h
+++ Core/Inc/car_utils.h
@@ -8,8 +8,8 @@
 #ifndef INC_CAR_UTILS_H_
 #define INC_CAR_UTILS_H_
 
-
 #include <stdbool.h>
+#include "main_task.h"
 
 typedef enum
 {
@@ -60,70 +60,39 @@ typedef enum
 
 } solenoid_position_t;
 
-// TODO these are not correct anymore
-
-void update_car_shift_struct();
-
-void clutch_task();
-
-void set_slow_drop(bool state);
-
-void set_clutch_solenoid(solenoid_position_t position);
-
-void set_upshift_solenoid(solenoid_position_t position);
-
-void set_downshift_solenoid(solenoid_position_t position);
-
-void set_shift_solenoids(solenoid_position_t position);
-
-void throttle_blip(bool state);
-
-void validate_throttle_blip();
-
-void spark_cut(bool state);
-
-void reach_target_RPM_spark_cut();
-
-void anti_stall();
-
-uint32_t read_neutral_sensor_pin();
-
-gear_t get_current_gear();
-
-uint32_t get_RPM();
-
-uint32_t get_target_RPM();
-
-void update_wheel_arr();
-
-void update_rpm_arr();
-
-float get_clutch_pot_pos();
-
-float get_shift_pot_pos();
-
-bool clutch_open();
-
-uint32_t get_trans_speed();
-
-float get_wheel_speed();
-
-float current_trans_wheel_ratio();
-
-float current_RPM_trans_ratio();
-
-
-uint32_t calc_target_RPM();
-
-bool validate_target_RPM();
-
-bool calc_validate_upshift();
-
-bool calc_validate_downshift();
-
-
 extern shift_struct_t car_shift_data;
 
+void update_car_shift_struct(void);
+void clutch_task(buttons_t* button_states, Main_States_t car_state, bool anti_stall_active);
+void check_buttons_and_set_clutch_sol(solenoid_position_t position, buttons_t* button_states);
+void check_and_spark_cut(bool state);
+void reach_target_RPM_spark_cut(uint32_t target_rpm);
+bool anti_stall(buttons_t* button_states, gear_t current_gear);
+gear_t get_current_gear(Main_States_t current_state);
+uint32_t calc_target_RPM(gear_t target_gear);
+bool validate_target_RPM(uint32_t target_rpm, gear_t target_gear, buttons_t* buttons);
+bool calc_validate_upshift(gear_t current_gear, buttons_t* buttons);
+bool calc_validate_downshift(gear_t current_gear, buttons_t* buttons);
+float get_ave_wheel_speed(uint32_t ms_of_samples);
+float get_ave_rpm(uint32_t ms_of_samples);
+void update_wheel_arr(void);
+void update_rpm_arr(void);
+float get_wheel_speed(void);
+float current_trans_wheel_ratio(void);
+float current_RPM_trans_ratio(void);
+uint32_t get_trans_speed(void);
+
+// HW functions
+void set_clutch_solenoid(solenoid_position_t position);
+void set_slow_drop(bool state);
+void set_upshift_solenoid(solenoid_position_t position);
+void set_downshift_solenoid(solenoid_position_t position);
+uint32_t read_neutral_sensor_pin(void);
+uint32_t get_RPM(void);
+float get_clutch_pot_pos(void);
+float get_shift_pot_pos(void);
+bool clutch_open(void);
+void set_spark_cut(bool state);
 
 
 #endif /* INC_CAR_UTILS_H_ */
--- Core/Src/car_utils.c
+++ Core/Src/car_utils.c
@@ -10,18 +10,31 @@
 #include "shift_parameters.h"
 #include "DAM.h"
 
-shift_struct_t car_shift_data = {.current_gear = ERROR_GEAR};
 extern TIM_HandleTypeDef htim2;
+
+shift_struct_t car_shift_data = {.current_gear = ERROR_GEAR};
+
+// TODO actually look at data for these and use some nice defines
+float gear_ratios[5] = {141.7f, 112.9f, 97.1f, 86.6f, 78.7f};
+
+// averaging RPM, wheel speed, and trans speed
+float rpm_arr[RPM_ARRAY_SIZE];
+float wheel_arr[WHEEL_SPEED_ARRAY_SIZE];
+uint32_t trans_speed_arr[TRANS_ARR_SIZE] = {0};
+uint32_t rpm_idx = 0;
+uint32_t wheel_idx = 0;
+uint32_t trans_idx = 0;
+
 
 // update_car_shift_struct
 //  Sets the current shifting state at the start of the loop so all of the data
 //  is latched for this loop
-void update_car_shift_struct()
+void update_car_shift_struct(void)
 {
-	car_shift_data.currently_moving = get_wheel_speed() > MOVING_WHEEL_SPEED_MIN_CUTOFF;
+	car_shift_data.currently_moving = get_ave_wheel_speed(DEFAULT_WHEEL_SPEED_AVE_TIME_ms) > MOVING_WHEEL_SPEED_MIN_CUTOFF;
 	car_shift_data.current_RPM = get_RPM();
 	car_shift_data.trans_speed = get_trans_speed();
-	car_shift_data.wheel_speed = get_wheel_speed();
+	car_shift_data.wheel_speed = get_ave_wheel_speed(DEFAULT_WHEEL_SPEED_AVE_TIME_ms);
 }
 
 
@@ -153,23 +166,23 @@ bool anti_stall(buttons_t* button_states, gear_t current_gear)
 	return car_shift_data.anti_stall;
 }
 
-// Gear calculation stuff
-float rpm_arr[RPM_ARRAY_SIZE];
-float wheel_arr[WHEEL_SPEED_ARRAY_SIZE];
-uint32_t trans_speed_arr[TRANS_ARR_SIZE] = {0};
-uint32_t rpm_idx = 0;
-uint32_t wheel_idx = 0;
-uint32_t trans_idx = 0;
-
-// TODO actually look at data for these
-//float gear_ratios[5] = {270.0f, 215.0f, 185.0f, 165.0f, 150.0f};
-float gear_ratios[5] = {141.7f, 112.9f, 97.1f, 86.6f, 78.7f};
-
 float rw_ratio_avg;
 float theoretical_rpm_arr[5];
+
+// get_current_gear
+//  returns the current gear the car is in. This is done first by checking the neutral
+//  sensor, then by using the rear wheel speed to find the correct ratio from the
+//  RPM to the wheels. If a gear has been established, it is unlikely that we
+//  changed gears so use more samples and take the closest based on the ratio.
+//  If the gear is not established then the gear ratios must be closer to the gear
 gear_t get_current_gear(Main_States_t current_state)
 {
-	uint32_t minimum_rpm_difference = 9999999.9f;
+	float minimum_rpm_difference = 9999999.9f;
+	float temp_diff;
+	float ave_wheel_speed;
+	float ave_rpm;
+	float theoredical_rpm;
+	uint8_t best_gear = 0;
 
 	// If we are currently shifting just use the last know gear
 	if (current_state == ST_HDL_UPSHIFT || current_state == ST_HDL_DOWNSHIFT)
@@ -197,155 +210,60 @@ gear_t get_current_gear(Main_States_t current_state)
 		return car_shift_data.current_gear;
 	}
 
-	// we must be in ST_IDLE state, clutch closed, and not moving. Find the gear we
-	// are closest to
-	// TODO
-	float wheel_sum = 0.0f;
-	float rpm_sum = 0.0f;
-	for (int i = 0; i < 100; i++)
+	if (car_shift_data.gear_established)
 	{
-		wheel_sum += wheel_arr[i];
-		rpm_sum += rpm_arr[i];
-	}
-	float wheel_avg  = wheel_sum / 1000.0f;
-	float rpm_avg = rpm_sum / 1000.0f;
-
-	// if the gear is not established, we must be close enough to a gear to establish
-	// it. This will use a smaller subset of samples
-	// TODO
-
-	// if the gear is established, use a much longer set of samples and take the
-	// closest gear
-	// TODO
-
-
-
-	// If we are shifting latch last known gear
-	if (car_Main_State == ST_HDL_UPSHIFT || car_Main_State == ST_HDL_DOWNSHIFT)
-	{
-		return car_shift_data.current_gear;
-	}
-	// TEMP REPLACEMENT
-	if (car_shift_data.currently_moving && car_Main_State == ST_IDLE)
-	{
-		float wheel_sum = 0.0f, rpm_sum = 0.0f;
-		for (int i = 0; i < 100; i++)
+		// if the gear is established, use a much longer set of samples and take the
+		// closest gear
+		ave_rpm = get_ave_rpm(GEAR_ESTABLISHED_NUM_SAMPLES_ms);
+		ave_wheel_speed = get_ave_wheel_speed(GEAR_ESTABLISHED_NUM_SAMPLES_ms);
+		for (uint8_t c = 0; c < NUM_OF_GEARS; c++)
 		{
-			wheel_sum += wheel_arr[i];
-			rpm_sum += rpm_arr[i];
-		}
-		float wheel_avg  = wheel_sum / 1000.0f;
-		float rpm_avg = rpm_sum / 1000.0f;
-
-		// TEMP DELETE
-		rw_ratio_avg = rpm_avg/wheel_avg;
-		for (int i = 0; i < 5; i++)
-		{
-			theoretical_rpm_arr[i] = wheel_avg * gear_ratios[i];
-		}
-		// END OF TEMP
-
-		for (int i = 0; i < 5; i++)
-		{
-			float theoretical_RPM = wheel_avg * gear_ratios[i];
-			if (	theoretical_RPM * (1.0 - GEAR_ESTABLISH_TOLERANCE_percent) < rpm_avg &&
-					rpm_avg  < theoretical_RPM * (1.0 + GEAR_ESTABLISH_TOLERANCE_percent))
+			theoredical_rpm = ave_wheel_speed * gear_ratios[c];
+			temp_diff = fabs(theoredical_rpm - ave_rpm);
+			if (temp_diff < minimum_rpm_difference)
 			{
-				car_shift_data.gear_established = true;
-				return (i + 1);
+				minimum_rpm_difference = temp_diff;
+				best_gear = c;
 			}
 		}
 
-		if (read_neutral_sensor_pin())
+		// we have found the minimum difference. Just return this gear
+		return (gear_t)(best_gear + 1);
+	}
+	else
+	{
+		// if the gear is not established, we must be close enough to a gear to establish
+		// it. This will use a smaller subset of samples
+		ave_rpm = get_ave_rpm(GEAR_NOT_ESTABLISHED_NUM_SAMPLES_ms);
+		ave_wheel_speed = get_ave_wheel_speed(GEAR_NOT_ESTABLISHED_NUM_SAMPLES_ms);
+		for (uint8_t c = 0; c < NUM_OF_GEARS; c++)
 		{
-			car_shift_data.gear_established = true;
-			return NEUTRAL;
+			theoredical_rpm = ave_wheel_speed * gear_ratios[c];
+			temp_diff = fabs(theoredical_rpm - ave_rpm);
+			if (temp_diff < minimum_rpm_difference)
+			{
+				minimum_rpm_difference = temp_diff;
+				best_gear = c;
+			}
 		}
 
-		// Not precise match up - could either be due to currently shifting or due to bad shift
-		return ERROR_GEAR;
-	}
-
-	// If not moving then check whether or not we have established current gear. If not then
-	// this is due to startup of the module and not having known position
-	if (!car_shift_data.gear_established)
-	{
-		// If we are not moving, the only way to establish our current gear is with neutral sensor
-		if (read_neutral_sensor_pin())
+		// we have found the minimum difference. If it is within tolerance then
+		// return the gear and establish. Otherwise return error gear and do not
+		// establish
+		if (temp_diff / ave_rpm <= GEAR_ESTABLISH_TOLERANCE_percent)
 		{
 			car_shift_data.gear_established = true;
-			return NEUTRAL;
+			return (gear_t)(best_gear + 1);
+		}
+		else
+		{
+			// no gear is good enough. Return error gear
+			return ERROR_GEAR;
 		}
 	}
 
-	// If gear is established simply return current gear. This is a dumb counter since we
-	// can't correct during a failed shift due to not moving. This will be incremented and
-	// decremented based on successful shifts. Default startup gear is ERROR_GEAR which will
-	// be returned by this function until position has been established by either neutral
-	// sensor or by starting to move. If a failed shift occurs we will return to ERROR_GEAR
-	// until position can be re-established
-	return car_shift_data.current_gear;
-}
-
-// update_wheel_arr
-//  Continuously add values to the wheel speed array whenever the clutch is not
-//  open
-void update_wheel_arr()
-{
-	// dont update the wheel or RPM array if the clutch is open. This will mean gear calculations
-	// will start from the last non-clutch samples, which is probably fine
-	if (clutch_open()) return;
-
-	wheel_arr[wheel_idx++] = get_wheel_speed();
-	wheel_idx = wheel_idx % WHEEL_SPEED_ARRAY_SIZE;
-}
-
-// update_rpm_arr
-//  Continuously add values to the RPM array whenever the clutch is not open
-void update_rpm_arr()
-{
-	// dont update the wheel or RPM array if the clutch is open. This will mean gear calculations
-    // will start from the last non-clutch samples, which is probably fine
-	if (clutch_open()) return;
-
-	rpm_arr[rpm_idx++] = rpm_ecu.data;
-	rpm_idx = rpm_idx % RPM_ARRAY_SIZE;
-}
-
-// get_wheel_speed
-//  returns the average rear wheel speed, or if one of the wheel speeds is bad
-//  returns only the good one
-float get_wheel_speed()
-{
-	// Handle bad data channel
-	if (fabs(wsrl_ecu.data) < 0.1f && fabs(wsrr_ecu.data) > 0.1f)
-	{
-		return (fabs(wsrr_ecu.data) < 0.1f) ? 0 : wsrr_ecu.data;
-	}
-	if (fabs(wsrr_ecu.data) < 0.1f && fabs(wsrl_ecu.data) > 0.1f)
-	{
-		return (fabs(wsrl_ecu.data) < 0.1f) ? 0 : wsrl_ecu.data;
-	}
-	float rear_wheel_avg = (wsrl_ecu.data + wsrr_ecu.data)/2.0f;
-	return (fabs(rear_wheel_avg) < 0.1f) ? 0 : rear_wheel_avg;
-}
-
-// current_trans_wheel_ratio
-//  Used for debugging/integration. Returns the current ratio between
-//  wheel speed and trans speed
-float current_trans_wheel_ratio()
-{
-	float rear_wheel_avg = get_wheel_speed();
-	return (fabs(rear_wheel_avg) < 1e-6f) ? -1.0f : (get_trans_speed()/rear_wheel_avg);
-}
-
-// current_RPM_trans_ratio
-//  Used for debugging/integration. Returns the current ratio between RPM and
-//  trans speed
-float current_RPM_trans_ratio()
-{
-	float trans_speed = get_trans_speed();
-	return (fabs(trans_speed) < 1e-6f) ? -1.0f : (rpm_ecu.data/trans_speed);
+	// not sure how we got here. Return ERROR_GEAR and panic
+	return ERROR_GEAR;
 }
 
 // calc_target_RPM
@@ -367,7 +285,7 @@ uint32_t calc_target_RPM(gear_t target_gear)
 	case GEAR_4:
 	case GEAR_5:
 		// This formula holds regardless of whether or not the clutch is pressed
-		return get_wheel_speed() * gear_ratios[target_gear - 1];
+		return get_ave_wheel_speed(DEFAULT_WHEEL_SPEED_AVE_TIME_ms) * gear_ratios[target_gear - 1];
 
 	case NEUTRAL:
 	case ERROR_GEAR:
@@ -427,11 +345,12 @@ bool calc_validate_upshift(gear_t current_gear, buttons_t* buttons)
 	case GEAR_4:
 		car_shift_data.target_gear = current_gear + 1;
 		car_shift_data.target_RPM = calc_target_RPM();
-		return validate_target_RPM();
+		// always allow shifts for now
+		//return validate_target_RPM();
+		return true;
 
 	case GEAR_5:
 	case ERROR_GEAR:
-		// If in ERROR_GEAR or GEAR_5 only allow shift if clutch override
 		car_shift_data.target_gear = ERROR_GEAR;
 		car_shift_data.target_RPM = 0;
 		return true;
@@ -452,7 +371,9 @@ bool calc_validate_downshift(gear_t current_gear, buttons_t* buttons)
 	case GEAR_5:
 		car_shift_data.target_gear = current_gear - 1;
 		car_shift_data.target_RPM = calc_target_RPM();
-		return validate_target_RPM();
+		// for now always allow downshifts, even if the target RPM is too high
+		//return validate_target_RPM();
+		return true;
 
 	case NEUTRAL:
 	case ERROR_GEAR:
@@ -463,29 +384,142 @@ bool calc_validate_downshift(gear_t current_gear, buttons_t* buttons)
 	}
 }
 
-// Transmission speed stuff
-
-// HAL_TIM_IC_CaptureCallback
-//  every time the IC interrupt is called, store the time between this callback
-//  and the last in a circular buffer
-void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
+// get_ave_wheel_speed
+//  Returns the the average wheel speed over a certain amount of time based on
+//  the parameter ms_of_samples. Limited to the size of the wheel speed array
+float get_ave_wheel_speed(uint32_t ms_of_samples)
 {
-	// TODO ensure that this is the correct way to ensure that the timer is correct
-	if (htim == &htim2)
+	int32_t starting_index;
+	float total = 0.0f;
+
+	// if more data points before are needed than the size of the array, cut it
+	// off
+	if (ms_of_samples > WHEEL_SPEED_ARRAY_SIZE)
 	{
-		if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1)
-		{
-			trans_speed_arr[trans_idx++] = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
-			trans_idx = trans_idx % TRANS_ARR_SIZE;
-		}
+		ms_of_samples = WHEEL_SPEED_ARRAY_SIZE;
 	}
+
+	// find the first point to average from. Rollover is possible
+	starting_index = wheel_idx - ms_of_samples;
+	if (starting_index > 0)
+	{
+		starting_index += WHEEL_SPEED_ARRAY_SIZE;
+	}
+
+	// count up the most recent values. Rollover must be accounted for
+	for (uint32_t c = 0; c < ms_of_samples; c++)
+	{
+		total += wheel_arr[(starting_index + c) % WHEEL_SPEED_ARRAY_SIZE];
+	}
+
+	// return the average
+	return (total / ms_of_samples);
 }
+
+// get_ave_rpm
+//  Returns the the average RPM over a certain amount of time based on
+//  the parameter ms_of_samples. Limited to the size of the RPM array
+float get_ave_rpm(uint32_t ms_of_samples)
+{
+	int32_t starting_index;
+	float total = 0.0f;
+
+	// if more data points before are needed than the size of the array, cut it
+	// off
+	if (ms_of_samples > RPM_ARRAY_SIZE)
+	{
+		ms_of_samples = RPM_ARRAY_SIZE;
+	}
+
+	// find the first point to average from. Rollover is possible
+	starting_index = rpm_idx - ms_of_samples;
+	if (starting_index > 0)
+	{
+		starting_index += RPM_ARRAY_SIZE;
+	}
+
+	// count up the most recent values. Rollover must be accounted for
+	for (uint32_t c = 0; c < ms_of_samples; c++)
+	{
+		total += rpm_arr[(starting_index + c) % RPM_ARRAY_SIZE];
+	}
+
+	// return the average
+	return (total / ms_of_samples);
+}
+
+// update_wheel_arr
+//  Continuously add values to the wheel speed array whenever the clutch is not
+//  open
+void update_wheel_arr(void)
+{
+	// dont update the wheel or RPM array if the clutch is open. This will mean gear calculations
+	// will start from the last non-clutch samples, which is probably fine
+	if (clutch_open()) return;
+
+	wheel_arr[wheel_idx++] = get_wheel_speed();
+	wheel_idx = wheel_idx % WHEEL_SPEED_ARRAY_SIZE;
+}
+
+// update_rpm_arr
+//  Continuously add values to the RPM array whenever the clutch is not open
+void update_rpm_arr(void)
+{
+	// dont update the wheel or RPM array if the clutch is open. This will mean gear calculations
+    // will start from the last non-clutch samples, which is probably fine
+	if (clutch_open()) return;
+
+	rpm_arr[rpm_idx++] = rpm_ecu.data;
+	rpm_idx = rpm_idx % RPM_ARRAY_SIZE;
+}
+
+// get_wheel_speed
+//  returns the average rear wheel speed, or if one of the wheel speeds is bad
+//  returns only the good one
+float get_wheel_speed(void)
+{
+	// if the left wheel is giving bad data
+	if (fabs(wsrl_ecu.data) < 0.1f && wsrr_ecu.data > 0.1f)
+	{
+		return wsrr_ecu.data;
+	}
+
+	// if the right wheel is giving bad data
+	if (fabs(wsrr_ecu.data) < 0.1f && wsrl_ecu.data > 0.1f)
+	{
+		return wsrl_ecu.data;
+	}
+
+	// both wheels are good
+	float rear_wheel_avg = (wsrl_ecu.data + wsrr_ecu.data) / 2.0f;
+	return (fabs(rear_wheel_avg) < 0.1f) ? 0 : rear_wheel_avg;
+}
+
+// current_trans_wheel_ratio
+//  Used for debugging/integration. Returns the current ratio between
+//  wheel speed and trans speed
+float current_trans_wheel_ratio(void)
+{
+	float rear_wheel_avg = get_wheel_speed();
+	return (fabs(rear_wheel_avg) < 1e-6f) ? -1.0f : (get_trans_speed()/rear_wheel_avg);
+}
+
+// current_RPM_trans_ratio
+//  Used for debugging/integration. Returns the current ratio between RPM and
+//  trans speed
+float current_RPM_trans_ratio(void)
+{
+	float trans_speed = get_trans_speed();
+	return (fabs(trans_speed) < 1e-6f) ? -1.0f : (rpm_ecu.data/trans_speed);
+}
+
+// Transmission speed stuff
 
 // get_trans_speed
 //  Return trans_speed from input capture in Hz. If no hall pulse within the last
 //  5ms returns 0 speed
 // TODO needs to be fully validated and reviewed still
-uint32_t get_trans_speed()
+uint32_t get_trans_speed(void)
 {
 	// Disable IC IT to prevent concurrent write/read
 	HAL_TIM_IC_Stop_IT(&htim2, TIM_CHANNEL_1);
@@ -528,6 +562,22 @@ uint32_t get_trans_speed()
 	return num_elems == 0 || sum_of_differences == 0 ? 0 : 16000000/(sum_of_differences/num_elems);
 }
 
+// HAL_TIM_IC_CaptureCallback
+//  every time the IC interrupt is called, store the time between this callback
+//  and the last in a circular buffer
+void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
+{
+	// TODO ensure that this is the correct way to ensure that the timer is correct
+	if (htim == &htim2)
+	{
+		if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1)
+		{
+			trans_speed_arr[trans_idx++] = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
+			trans_idx = trans_idx % TRANS_ARR_SIZE;
+		}
+	}
+}
+
 // Direct get/set functions
 
 void set_clutch_solenoid(solenoid_position_t position)
@@ -550,27 +600,27 @@ void set_downshift_solenoid(solenoid_position_t position)
 	HAL_GPIO_WritePin(DOWNSHIFT_SOL_GPIO_Port, DOWNSHIFT_SOL_Pin, position);
 }
 
-uint32_t read_neutral_sensor_pin()
+uint32_t read_neutral_sensor_pin(void)
 {
 	return !HAL_GPIO_ReadPin(NTRL_SW_GPIO_Port, NTRL_SW_Pin);
 }
 
-uint32_t get_RPM()
+uint32_t get_RPM(void)
 {
 	return rpm_ecu.data;
 }
 
-float get_clutch_pot_pos()
+float get_clutch_pot_pos(void)
 {
 	return tcm_clutch_position.data;
 }
 
-float get_shift_pot_pos()
+float get_shift_pot_pos(void)
 {
 	return tcm_shifter_position.data;
 }
 
-bool clutch_open()
+bool clutch_open(void)
 {
 	return get_clutch_pot_pos() < CLUTCH_OPEN_POS_MM;
 }
